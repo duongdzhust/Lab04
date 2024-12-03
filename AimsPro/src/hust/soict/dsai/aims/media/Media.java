1ef288fea381f2dd35bcd1b4e202811f210b9829
Pham <phamduong121104@gmail.com>
2024-12-03 20:37:16 +0700

    overide

--- AimsPro/src/hust/soict/dsai/aims/media/Media.java
+++ AimsPro/src/hust/soict/dsai/aims/media/Media.java
@@ -6,39 +6,37 @@ public abstract class Media {
     private String category;
     private float cost;
 
-    // Constructor
     public Media() {}
 
-    // Getter và Setter
-    public int getId() {
-        return id;
+    public Media(int id, String title, String category, float cost) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public int getId() {
+        return id;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getCategory() {
         return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
     }
 
     public float getCost() {
         return cost;
     }
 
-    public void setCost(float cost) {
-        this.cost = cost;
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Media media = (Media) obj;
+        return title != null && title.equals(media.title);
     }
 }
+
